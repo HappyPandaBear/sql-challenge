346ff0cb3582c34312c3834796084c01bb899f9d
HappyPandaBear <76534442+HappyPandaBear@users.noreply.github.com>
2021-05-15 14:32:03 -0700

    fixed git space errors
    fixed errors where git make spacing or indents weird.

--- EmployeeSQL/schemas_tables.sql
+++ EmployeeSQL/schemas_tables.sql
@@ -13,8 +13,8 @@ SELECT * FROM departments;
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
     "dept_no" varchar   NOT NULL,
-	"from_date" date	NOT NULL,
-	"to_date" date	NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
 	CONSTRAINT pk_dept_emp PRIMARY KEY (emp_no,dept_no));
 
 SELECT * FROM dept_emp;
@@ -24,8 +24,8 @@ SELECT * FROM dept_emp;
 CREATE TABLE "dept_manager" (
     "dept_no" varchar   NOT NULL,
     "emp_no" int   NOT NULL,
-	"from_date" date	NOT NULL,
-	"to_date" date	NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
 	CONSTRAINT pk_dept_manager PRIMARY KEY (dept_no,emp_no));
 	
 SELECT * FROM dept_manager;
@@ -48,8 +48,8 @@ SELECT * FROM employees;
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
     "salary" int   NOT NULL,
-	"from_date" date	NOT NULL,
-	"to_date" date	NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
 	CONSTRAINT pk_salaries PRIMARY KEY (emp_no));
 	
 SELECT * FROM salaries;
@@ -59,13 +59,15 @@ SELECT * FROM salaries;
 CREATE TABLE "titles" (
     "emp_no" int   NOT NULL,
     "title" varchar   NOT NULL,
-	"from_date" date	NOT NULL,
-	"to_date" date	NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
     PRIMARY KEY (emp_no, from_date),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no));
 
 SELECT * FROM titles;
 
+--						  
+						  
 select * from departments;
 select * from dept_emp;
 select * from dept_manager;
